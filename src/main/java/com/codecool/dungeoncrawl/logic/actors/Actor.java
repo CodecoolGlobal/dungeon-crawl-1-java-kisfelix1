7342f78e07e22731026b732842a61e8622dfe1df
Kis Béla <kisfelix1@gmail.com>
2021-12-14 14:53:51 +0100

    Feature - player can attack monster

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -15,15 +15,36 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (canBeSteppedAt(nextCell)) {
+        if(isActorOnCell(nextCell)){
+            attackActor(nextCell.getActor());
+        }
+        else if (!isWallOnCell(nextCell)) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
         }
     }
 
-    private boolean canBeSteppedAt(Cell nextCell) {
-        return !nextCell.getType().equals(CellType.WALL) && nextCell.getActor() == null;
+    protected void attackActor(Actor actorToAttack){
+        actorToAttack.modifyHealth(-10);
+        if (actorToAttack.getCell().getActor() != null){
+            modifyHealth(-5);
+        }
+    }
+
+    public void modifyHealth(int amountOfHealth){
+        health += amountOfHealth;
+        if (health <= 0){
+            cell.setActor(null);
+        }
+    }
+
+    private boolean isActorOnCell(Cell nextCell) {
+        return nextCell.getActor() != null;
+    }
+
+    private boolean isWallOnCell(Cell nextCell) {
+        return nextCell.getType().equals(CellType.WALL);
     }
 
     public int getHealth() {
